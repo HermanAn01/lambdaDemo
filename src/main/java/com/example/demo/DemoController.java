e5090fd5c3d5b84d360d9548a47ed330fe925434
herman.anecmarkets.com <herman.an@ecmarkets.com>
2025-03-17 18:39:15 +1300

    init

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import net.spotec.lambda.LambdaEnum;
+import net.spotec.lambda.LambdaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller("demo")
+public class DemoController {
+
+    @Resource
+    private LambdaService lambdaService;
+
+    @RequestMapping("test")
+    public void demo(String publickey){
+
+        String res = lambdaService.sendRequest(LambdaEnum.LB_SM);
+        System.out.println(res);
+
+        try {
+            String ret = lambdaService.decryptForBc(res, publickey);
+            System.out.println(ret);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("demo");
+    }
+}
