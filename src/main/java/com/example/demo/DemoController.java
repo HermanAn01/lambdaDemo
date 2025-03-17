d99029599519899068920220ef781e66fdc123f1
herman.anecmarkets.com <herman.an@ecmarkets.com>
2025-03-17 19:00:50 +1300

    init

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,17 +1,21 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@Controller("demo")
+
+@RestController
+@RequestMapping("/demo")
 public class DemoController {
 
     @Resource
     private LambdaService lambdaService;
 
-    @RequestMapping("test")
+    @GetMapping("test")
     public void demo(String publickey){
 
         String res = lambdaService.sendRequest(LambdaEnum.LB_SM);
