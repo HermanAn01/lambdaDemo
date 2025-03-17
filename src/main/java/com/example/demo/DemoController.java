935aa77b213ce478bf111052ebbed6f98bf71ff8
herman.anecmarkets.com <herman.an@ecmarkets.com>
2025-03-18 00:03:40 +1300

    init

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 @RestController
@@ -19,6 +23,12 @@ public class DemoController {
 
     @PostMapping("test")
     public void demo(@RequestBody Bean bean){
+        String publicIp = getPublicIp();
+        String privateIp = getPrivateIp();
+
+        System.out.println("Public IP: " + publicIp);
+        System.out.println("Private IP: " + privateIp);
+
 
         String res = lambdaService.sendRequest(LambdaEnum.LB_SM);
         System.out.println(res);
@@ -35,5 +45,33 @@ public class DemoController {
         System.out.println("demo");
     }
 
+
+    // 获取公共 IP 地址
+    public static String getPublicIp() {
+        return getMetadata("http://169.254.169.254/latest/meta-data/public-ipv4");
+    }
+
+    // 获取私有 IP 地址
+    public static String getPrivateIp() {
+        return getMetadata("http://169.254.169.254/latest/meta-data/local-ipv4");
+    }
+
+    // 获取指定元数据
+    private static String getMetadata(String metadataUrl) {
+        try {
+            URL url = new URL(metadataUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String ip = reader.readLine();
+            reader.close();
+
+            return ip;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
